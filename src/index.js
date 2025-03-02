24a8adff29e0d489b7f0bfc56e0b112e7fc7642e
FearQuare <elmira.aras@protonmail.com>
2025-03-02 04:19:10 +0300

    new workspace click fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import "./styles.css";
 import MainPage from "./modules/mainPage";
-import Navbar  from "./modules/ui/navbar";
+import Navbar from "./modules/ui/navbar";
 import Sidebar from "./modules/ui/sidebar";
 import WorkspacePage from "./modules/workspacePage";
+
+function addWorkspaceEventListeners() {
+    const workspaces = document.querySelectorAll(".workspace");
+    workspaces.forEach(workspace => {
+        workspace.addEventListener("click", () => {
+            app.appendChild(WorkspacePage(workspace.id));
+        });
+    });
+}
 
 const app = document.querySelector("#app");
 app.appendChild(Navbar());
 app.appendChild(Sidebar());
 app.appendChild(MainPage());
 
-const workspaces = document.querySelectorAll(".workspace");
-workspaces.forEach(workspace => {
-    workspace.addEventListener("click", () => {
-        app.appendChild(WorkspacePage(workspace.id));
-    });
-});
+addWorkspaceEventListeners();
+
+const addWorkspaceButton = document.querySelector(".add-workspace-button");
+addWorkspaceButton.addEventListener("click", addWorkspaceEventListeners);
 
 const logo = document.querySelector(".todo-logo");
 logo.addEventListener("click", () => {
