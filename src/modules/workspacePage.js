3e32ead98120f3c5396941febe0fcba6c329db79
FearQuare <elmira.aras@protonmail.com>
2025-03-02 21:51:41 +0300

    add edit workspace name button

--- src/modules/workspacePage.js
+++ src/modules/workspacePage.js
@@ -1,6 +1,7 @@
 // actually, instead of getting all the workspaces each time, I can write a function which gets the specific
 // workspace by id
 import { getExistingWorkspaces } from "./workspaces/workspaceController";
+import EditIcon from "../assets/icons/edit-icon.svg";
 
 export default function WorkspacePage(workspaceId) {
     // get the previous div and remove it because we need space to show our workspace
@@ -17,9 +18,26 @@ export default function WorkspacePage(workspaceId) {
     const workspaceToBeDisplayed = workspaces[workspaceId];
 
     // display the name of the choosen workspace
-    const heading = document.createElement("div");
-    heading.innerText = workspaceToBeDisplayed.name;
-    workspaceComponent.appendChild(heading);
+    const nameSection = document.createElement("div");
+    const name = document.createElement("h1");
+    const editButton = document.createElement("button");
+    const editIcon = document.createElement("img");
+
+    editButton.classList.add("edit-button");
+
+
+    editIcon.src = EditIcon;
+    editIcon.alt = "Edit Icon";
+
+    name.innerText = workspaceToBeDisplayed.name;
+
+    nameSection.classList.add("workspace-name");
+    nameSection.appendChild(name);
+
+    editButton.appendChild(editIcon);
+    nameSection.appendChild(editButton);
+
+    workspaceComponent.appendChild(nameSection);
 
     return workspaceComponent;
 }
