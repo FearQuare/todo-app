9261534e09227ec6153b664f15638afb2a185f59
FearQuare <elmira.aras@protonmail.com>
2025-03-23 00:18:07 +0300

    Deployment commit

--- src/modules/workspaces/sections/sectionStorage.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = "sections";
-
-export function saveSections(section) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(section));
-}
-
-export function getSections() {
-    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
-}
--- src/modules/workspaces/workspace.js
+++
@@ -1,9 +0,0 @@
-// Workspace class
-// Workspaces should have: name, id, and sections
-
-export default class Workspace {
-    constructor (name, id) {
-        this.name = name;
-        this.id = id;
-    }
-}
